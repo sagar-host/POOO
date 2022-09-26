687217c608582e9a1d37c986da419713aaa56489
sagar <92850073+sagar-host@users.noreply.github.com>
2022-09-26 16:01:23 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -142,7 +142,7 @@ const Main = () => {
            
             <Center click={click}>
                 <YinYang  onClick={()=> handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' />
-                <span>click here</span>
+                <span></span>
             </Center>
 
             <Contact target="_blank" to={{pathname:"mailto:rebelsagarx@gmail.com"}}>
