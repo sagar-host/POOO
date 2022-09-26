a9884a9768f3b5947ce0390373155fc04e423150
sagar <92850073+sagar-host@users.noreply.github.com>
2022-09-26 15:51:28 +0530

    Update SoundBar.js

--- src/subComponents/SoundBar.js
+++ src/subComponents/SoundBar.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import styled, { keyframes } from 'styled-components'
 
-import music from "../assets/audio/Halloween.mp3"
+import music from "../assets/Halloween.mp3"
 
 const Box = styled.div`
 display:flex;
